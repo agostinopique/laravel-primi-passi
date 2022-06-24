847cab945f2246bd281e251d5200797037ee6df8
agostinopique <ago.pique@yahoo.it>
2022-06-24 11:08:37 +0200

    Fix route dei vari pulsanti di navigazione;

--- routes/web.php
+++ routes/web.php
@@ -13,40 +13,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    $data = [
-        'user' => 'Giovanni Giorgio',
-        'message' => 'Ciao amico!',
-        'menu' => ['Home', 'About', 'Contacts'],
-        'lorem' =>'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque placeat iste perspiciatis nisi similique, facilis repellat aut quisquam voluptas id maxime? Asperiores doloremque aut veritatis quibusdam eum enim assumenda animi atque perspiciatis soluta? Repellendus harum sint accusamus, aut tenetur animi modi necessitatibus voluptate iure repellat! Error nobis culpa pariatur enim facilis exercitationem voluptates repellendus quam aliquid reprehenderit saepe distinctio nostrum deleniti iure repudiandae magnam ipsum, quo officiis ex doloremque corrupti hic accusantium quae? Pariatur reiciendis, ullam dicta tempore, sed provident laudantium explicabo animi accusamus dignissimos alias, fuga molestias corrupti placeat facere id inventore consectetur assumenda officiis sapiente esse nesciunt. Vitae.'
-    ];
-    return view('home', $data);
-})->name('home');
-
 Route::get('/Home', function () {
     $data = [
+
         'user' => 'Giovanni Giorgio',
         'message' => 'Ciao amico!',
         'menu' => ['Home', 'About', 'Contacts'],
         'lorem' =>'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque placeat iste perspiciatis nisi similique, facilis repellat aut quisquam voluptas id maxime? Asperiores doloremque aut veritatis quibusdam eum enim assumenda animi atque perspiciatis soluta? Repellendus harum sint accusamus, aut tenetur animi modi necessitatibus voluptate iure repellat! Error nobis culpa pariatur enim facilis exercitationem voluptates repellendus quam aliquid reprehenderit saepe distinctio nostrum deleniti iure repudiandae magnam ipsum, quo officiis ex doloremque corrupti hic accusantium quae? Pariatur reiciendis, ullam dicta tempore, sed provident laudantium explicabo animi accusamus dignissimos alias, fuga molestias corrupti placeat facere id inventore consectetur assumenda officiis sapiente esse nesciunt. Vitae.'
     ];
     return view('home', $data);
-})->name('home');
+})->name('Home');
 
 
 Route::get('/About', function () {
     $data = [
+
         'menu' => ['Home', 'About', 'Contacts'],
         'lorem' =>'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque placeat iste perspiciatis nisi similique, facilis repellat aut quisquam voluptas id maxime? Asperiores doloremque aut veritatis quibusdam eum enim assumenda animi atque perspiciatis soluta? Repellendus harum sint accusamus, aut tenetur animi modi necessitatibus voluptate iure repellat! Error nobis culpa pariatur enim facilis exercitationem voluptates repellendus quam aliquid reprehenderit saepe distinctio nostrum deleniti iure repudiandae magnam ipsum, quo officiis ex doloremque corrupti hic accusantium quae? Pariatur reiciendis, ullam dicta tempore, sed provident laudantium explicabo animi accusamus dignissimos alias, fuga molestias corrupti placeat facere id inventore consectetur assumenda officiis sapiente esse nesciunt. Vitae.'
     ];
     return view('about', $data);
-})->name('about');
+})->name('About');
 
 
 Route::get('/Contacts', function () {
     $data = [
+
         'menu' => ['Home', 'About', 'Contacts'],
         'contatti' => ['email@libero.it', 05555552, 'via santi santissimi']
     ];
     return view('contacts', $data);
-})->name('contacts');
+})->name('Contacts');
